5870d5f56205ad0bfbfe7a5f71f2da64d3601649
lixuebin <lixuebin@cdeledu.com>
2021-04-20 15:36:37 +0800

    add synchronize send

--- src/main/java/llxxbb/demo/test/Producer.java
+++ src/main/java/llxxbb/demo/test/Producer.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 import static llxxbb.demo.kafka.TopicConfig.TOPIC;
 
 @RestController // 可以是其它注解，这里只是方便测试
@@ -21,14 +23,16 @@ public class Producer {
     }
 
     @RequestMapping("/user/{name}")  // 测试用
-    public void sendUser(@PathVariable("name") String name) {
+    public void sendUser(@PathVariable("name") String name) throws ExecutionException, InterruptedException {
         User<UserOtherInfo> user = new User<>();
         user.name = name;
         user.age = 100;
         user.other = new UserOtherInfo();
         user.other.address = "china";
         user.other.phone = "88888888888";
+        // 异步发送
         template.send("user", user);
+        // 同步发送
+        template.send("user", user).get();
     }
-
 }
